ecf477ea1480fc5a092e1bb1a4b1c6f48d21e3cc
Faizan Bhatti <faizandota@gmail.com>
2026-02-13 19:15:34 +0500

    App.js added

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      
       <header className="App-header">
+        <h1>Inventory Analyzer</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
